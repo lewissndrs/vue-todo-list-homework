f38188073d6d3ff164e5f19583376fafda31d3f7
lewissndrs <lewissndrs@gmail.com>
2020-10-12 16:46:44 +0100

    finished MVP and extensions

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>To Do's</h1>
+
+    <form v-on:submit.prevent="addToList">
+      <label for="name">Name</label>
+      <input type="text" id='name' v-model="newItem">
+      
+      <label for="priority">Priority: </label>
+      <input type="radio" name='priority' value='high' v-model="priorityTrack">
+      <label for="high">High</label>
+      <input type="radio" name='priority' value='low' v-model="priorityTrack">
+      <label for="low">Low</label>
+
+      <input type="submit" id="submit">
+    </form>
+
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index" v-bind:class="todo.hiPriority ? 'hi' : 'lo'">
+        <span>{{ todo.name }}</span>
+        </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      todos : [{name:"Buy a parrot",hiPriority:false},{name:"Love oneself",hiPriority:true},{name:"clean potatoes",hiPriority:false}],
+      newItem : "",
+      priorityTrack: ""
+    }
+  },
+  methods: {
+    addToList: function() {
+
+      let priority = null;
+
+        if (this.priorityTrack === "high") {
+          priority = true;
+        } else {
+          priority = false;
+        };
+
+      this.todos.push({
+        name:this.newItem,
+        hiPriority:priority
+        });
+      this.newItem = "";
+      this.priorityTrack = "";
+    }
   }
 }
 </script>
@@ -24,5 +63,50 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 25px;
+}
+
+form {
+  text-align: start;
+  width: 55%;
+  margin: auto;
+}
+
+form > input, form > label {
+  padding: 5px;
+  margin: 5px;
+  font-size: 25px;
+}
+
+#submit {
+  background-color: black;
+  color: white;
+  font-size: 25px;
+  border-radius: 0;
+  padding: 5px;
+}
+
+li {
+  list-style-type: none;
+  padding: 10px;
+  text-align: start;
+  width: 60%;
+  margin: 12px auto;
+}
+
+li > span {
+  padding-left: 3%;
+}
+
+.hi {
+  color: red;
+  border: 2px solid red;
+}
+
+.lo {
+  color: dodgerblue;
+  border: 2px solid dodgerblue;
 }
 </style>
+
+
